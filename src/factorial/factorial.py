3471cef8bac2686326ec4aa074113046cfc20c92
Juan Acuña <juanacuna@MacBook-de-Juan.local>
2025-03-26 18:49:39 -0300

    Agregue script factorial.py y lo modifique para que solicite un numero

---
+++ src/factorial/factorial.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+#*-------------------------------------------------------------------------*
+#* factorial.py                                                            *
+#* calcula el factorial de un número                                       *
+#* Dr.P.E.Colla (c) 2022                                                   *
+#* Creative commons                                                        *
+#*-------------------------------------------------------------------------*
+import sys
+def factorial(num): 
+    if num < 0: 
+        print("Factorial de un número negativo no existe")
+        return 0
+    elif num == 0: 
+        return 1
+        
+    else: 
+        fact = 1
+        while(num > 1): 
+            fact *= num 
+            num -= 1
+        return fact 
+
+if len(sys.argv) == 1:
+   num = int(input("Debe informar un número: "))
+else:
+   num = int(sys.argv[1])
+
+num=int(sys.argv[1])
+print("Factorial ",num,"! es ", factorial(num)) 
+
